8c982492556b88ed32e998eaea160544709c60e2
Luiz_E <luizedumartins21@gmail.com>
2024-10-09 22:15:14 -0300

    start project

---
+++ src/main/java/com/luizedu/app_acougue/AppAcougueApplication.java
@@ -0,0 +1,13 @@
+package com.luizedu.app_acougue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAcougueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAcougueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizedu/app_acougue/AppAcougueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizedu.app_acougue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAcougueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
