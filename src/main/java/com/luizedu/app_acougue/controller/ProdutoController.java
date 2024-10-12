38c2889d533931ca34b3956c99721d9878774eac
Luiz_E <luizedumartins21@gmail.com>
2024-10-11 21:46:39 -0300

    criar produto

--- src/main/java/com/luizedu/app_acougue/controller/ProdutoController.java
+++ src/main/java/com/luizedu/app_acougue/controller/ProdutoController.java
@@ -1,8 +1,11 @@
 package com.luizedu.app_acougue.controller;
 
+import com.luizedu.app_acougue.model.Produto;
 import com.luizedu.app_acougue.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +16,9 @@ public class ProdutoController {
     @Autowired
     ProdutoService produtoService;
 
+    @PostMapping("/criarProduto")
+    public ResponseEntity<Produto> salvar(@RequestBody Produto produto) {
+        produtoService.salvar(produto);
+        return ResponseEntity.ok(produto);
+    }
 }
